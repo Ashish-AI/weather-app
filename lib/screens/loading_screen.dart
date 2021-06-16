54d6dcfb5247e56d14ef27a2c7ecce4c7ff83032
Ashish Chaubey <ashishchaubey866@gmail.com>
2021-06-16 17:35:13 +0530

    API sample added for temp, city and condition

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,5 +1,8 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:weather_app/services/location.dart';
+import 'package:http/http.dart' as http;
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -20,7 +23,34 @@ class _LoadingScreenState extends State<LoadingScreen> {
     print(location.longitude);
   }
 
+  void getData() async {
+    http.Response response = await http.get(Uri.parse(
+        'https://samples.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=b6907d289e10d714a6e88b30761fae22'));
+    if (response.statusCode == 200) {
+      String data = response.body;
+      // var longitude = jsonDecode(data)['coord']['lon'];
+      // print(longitude);
+
+      var decodedData = jsonDecode(data);
+
+      double temp = jsonDecode(data)['main']['temp'];
+      //or
+      int condition = decodedData['weather'][0]['id'];
+      String cityName = decodedData['name'];
+      print(temp);
+      print(condition);
+      print(cityName);
+    } else {
+      print(response.statusCode);
+    }
+
+    // print(response);
+    // print(response.body);
+    // print(response.statusCode);
+  }
+
   Widget build(BuildContext context) {
+    getData();
     return Scaffold(
       body: Center(),
     );
